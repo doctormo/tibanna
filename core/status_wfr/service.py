246905fa2364e53eb786d9bac2f3da6840e241e0
j1z0 <jeremy@softworks.com.my>
2018-02-14 16:07:20 -0500

    check status of wfr

---
+++ core/status_wfr/service.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+import boto3
+
+
+def handler(event, context):
+    arn = event['executionArn']
+    # see if task is running and kill it
+    client = boto3.client('stepfunctions', region_name='us-east-1')
+    run_details = client.describe_execution(
+            executionArn=arn
+        )
+    run_details['startDate'] = str(run_details['startDate'])
+    run_details['stopDate'] = str(run_details['stopDate'])
+    return run_details
+
+if __name__ == "__main__":
+    handler({'executionArn': 'arn:aws:states:us-east-1:643366669028:execution:tibanna_pony-dev:test'}, 1)
